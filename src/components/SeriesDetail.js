183bb68e3b164dffadab42cc66fb7c92ba305b6a
Hector F <hectorf.@Hectors-MacBook-Pro.local>
2018-11-23 17:55:49 -0500

    remove styling

--- src/components/SeriesDetail.js
+++ src/components/SeriesDetail.js
@@ -7,13 +7,8 @@ import CardSection from './CardSection'
 const SeriesDetail = ({serie}) => {
     const {url,title,user_id}= serie
     const {
-        headerContentStyle,
-        thumbnailContainerStyle,
-        thumbnailStyle,
-        headerTextStyle,
         image,
         imageText,
-        center
     } = styles
 
     return(
@@ -22,7 +17,7 @@ const SeriesDetail = ({serie}) => {
                 <ImageBackground
                     style={image}
                     source={{ uri: url }}
-                    blurRadius={6}>
+                    blurRadius={4}>
                     <Text style={imageText}>
                         {title}
                     </Text>
@@ -33,26 +28,6 @@ const SeriesDetail = ({serie}) => {
 }
 
 const styles = {
-    center:{
-        flex:1
-    },
-    headerContentStyle:{
-        flexDirection:'column',
-        justifyContent:'space-around'
-    },
-    headerTextStyle:{
-        fontSize:18
-    },
-    thumbnailStyle:{
-        height:50,
-        width:50
-    },
-    thumbnailContainerStyle:{
-        justifyContent:'center',
-        alignItems:'center',
-        marginLeft:10,
-        marginRight:10
-    },
     image:{
         height:300,
         flex: 1,
@@ -61,8 +36,9 @@ const styles = {
         alignItems: 'center'
     },
     imageText:{
-        fontSize:30,
-        color:'#fff'
+        fontSize:50,
+        color:'#fff',
+        fontFamily:'Cochin'
     }
 }
 
