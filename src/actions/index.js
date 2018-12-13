59ef117caccfa70f587aede829abc57402de180b
Hector F <hectorf.@Hectors-MacBook-Pro.local>
2018-12-13 15:20:56 -0500

    rename clear email action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED, CLEAR_EMAIL_STATE} from './types'
+import {EMAIL_CHANGED, PASSWORD_CHANGED, CLEAR_EMAIL} from './types'
 export const onEmailChanged = (text) => {
     return{
         type: EMAIL_CHANGED,
@@ -13,9 +13,16 @@ export const onPasswordChanged = (text) => {
     }
 }
 
-export const clearEmailState = () => {
+export const clearEmail = () => {
     return{
-        type:CLEAR_EMAIL_STATE,
+        type:CLEAR_EMAIL,
         payload:""
+    }
+}
+
+export const clearPasswordState = () => {
+    return {
+        type: CLEAR_EMAIL_STATE,
+        payload: ""
     }
 }
