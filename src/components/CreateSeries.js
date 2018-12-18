9338de2a989c302fcf701b39a91c0e74b053ff92
Hector F <hectorf.@Hectors-MacBook-Pro.local>
2018-12-17 20:53:46 -0500

    create add series form

--- src/components/CreateSeries.js
+++ src/components/CreateSeries.js
@@ -1,18 +1,47 @@
 import React,{Component} from 'react'
-import {Text,View} from 'react-native'
+import {Text} from 'react-native'
+import { connect } from 'react-redux'
+import { onSeriesUpdate } from '../actions'
+import { Card, CardSection, Button, Spinner, Input} from './common'
 
 
 class CreateSeries extends Component {
 
+    onSeriesUpdate(text){
+        this.props.onSeriesUpdate({prop:'seriesTitle', value:text})
+    }
+
     render(){
         return(
-            <View>
-                <Text>
-                    Create Series
-                </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        onChangeText={this.onSeriesUpdate.bind(this)}
+                        label={"Title"}
+                        value={this.props.seriesTitle}
+                    >
+                    </Input>
+                </CardSection>
+
+                <CardSection>
+                    <Text>
+                        {this.props.seriesTitle}
+                    </Text>
+                </CardSection>
+
+                <CardSection>
+                    <Button
+                        size={"small"}
+                        buttonText={"Create"}
+                    />
+                </CardSection>
+            </Card>
         )
     }
 }
 
-export default CreateSeries
+const mapStateToProps=({series})=>{
+    const {seriesTitle} = series
+    return{ seriesTitle }
+}
+export default connect(mapStateToProps,{onSeriesUpdate})(CreateSeries)
